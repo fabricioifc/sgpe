d8a554a6b46dae344e9bb48c5a977f53a085f237
= <=>
2017-06-22 11:28:17 -0300

    commit

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,12 @@ module Jogosnoif
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.encoding = "utf-8"
+    config.time_zone = 'Brasilia'
+
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.available_locales = [:en, :"pt-BR"]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
