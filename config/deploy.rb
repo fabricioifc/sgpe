57f89fad1d3c1230c48b4be27e7b59905236c6d4
Fabricio <fabricio.bizotto@gmail.com>
2017-08-14 02:13:59 -0300

    comit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,31 +1,43 @@
-require 'mina/multistage'
 require 'mina/rails'
 require 'mina/git'
 require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (https://rvm.io)
 require 'mina/puma'
 
+set :application_name, 'sgpe'
+set :domain, "200.135.61.15"
+set :deploy_to, "/home/deploy/sgpe_production"
+set :repository, "git@bitbucket.org:fraiburgoifc/planodeensinoifc.git"
+set :branch, "master"
+set :port, '50235'
+
+set :user, "deploy"
+set :forward_agent, true
+
 set :shared_dirs, fetch(:shared_dirs, []).push('log', 'pids', 'sockets', 'public/uploads')
 set :shared_files, fetch(:shared_files, []).push(
   'config/database.yml', 'config/secrets.yml', 'config/puma.rb',
   '.env.test', '.env.development', '.env.staging', '.env.production'
 )
-
 desc "Deploys the current version to the server."
-task deploy: :environment do
-  comment "Deploying #{fetch(:application_name)} to #{fetch(:domain)}:#{fetch(:deploy_to)}"
-  invoke :'git:clone'
-  invoke :'deploy:link_shared_paths'
-  # invoke :'rvm:load_env_vars'
-  invoke :'bundle:install'
-  invoke :'rails:db_migrate'
-  command %{#{fetch(:rails)} db:seed}
-  invoke :'rails:assets_precompile'
-  invoke :'deploy:cleanup'
+task :production do
+  deploy do
+    # set :unicorn_env, 'production'
+    set :rails_env, 'production'
+    comment "Deploying #{fetch(:application_name)} to #{fetch(:domain)}:#{fetch(:deploy_to)}"
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    # invoke :'rvm:load_env_vars'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    command %{#{fetch(:rails)} db:seed}
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
 
-  on :launch do
-    invoke :'puma:phased_restart'
-    # invoke :'unicorn:restart'
+    on :launch do
+      invoke :'puma:phased_restart'
+      # invoke :'unicorn:restart'
+    end
   end
 end
 
@@ -34,7 +46,7 @@ task :environment do
   # invoke :'rvm:use', 'ruby-2.4.0@default'
 end
 
-task setup: :environment do
+task :setup do
   command %[mkdir -p "#{fetch(:shared_path)}/pids"]
   command %[mkdir -p "#{fetch(:shared_path)}/log"]
   command %[mkdir -p "#{fetch(:shared_path)}/sockets"]
