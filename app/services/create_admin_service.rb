4c602b113671b07f8d26fbfb91c775d294db757c
Fabricio <fabricio.bizotto@gmail.com>
2017-08-16 23:25:07 -0300

    user

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,10 +1,8 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by(
-        email: Rails.application.secrets.admin_email,
-        name:  Rails.application.secrets.admin_name,
-        username:  Rails.application.secrets.admin_username,
-      ) do |user|
+    user = User.find_or_create_by(email: Rails.application.secrets.admin_email) do |user|
+      user.username                   = Rails.application.secrets.admin_username
+      user.name                   = Rails.application.secrets.admin_name
       user.password               = Rails.application.secrets.admin_password
       user.password_confirmation  = Rails.application.secrets.admin_password
       user.admin                  = true
