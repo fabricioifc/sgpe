9b96f269638e5f32ed2e464485e53ca668b1bcc6
Fabricio <fabricio.bizotto@gmail.com>
2017-09-06 12:05:43 -0300

    perfis seed

---
+++ app/services/create_perfil_service.rb
@@ -0,0 +1,15 @@
+class CreatePerfilService
+
+  def call
+
+    perfils = []
+    perfils << [ name: 'DDE', idativo: true ]
+    perfils << [ name: 'CGE', idativo: true ]
+
+    perfils.each do |k, v|
+      Perfil.find_or_create_by!(k)
+    end
+
+    puts "Lista de perfis atualizada!"
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,5 @@ puts 'CREATED ADMIN USER: ' << user.email
 CreateCourseService.new.call
 # Criar disciplinas
 CreateDisciplineService.new.call
+# Criar perfis
+CreatePerfilService.new.call
