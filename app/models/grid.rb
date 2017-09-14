654dc63d9b9bd189a95af40f2543b09f7da64454
Fabricio <fabricio.bizotto@gmail.com>
2017-09-14 10:26:41 -0300

    painel nested forms working

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -1,12 +1,12 @@
 class Grid < ApplicationRecord
   belongs_to :course
   belongs_to :user
-  has_many :grid_disciplines
-  has_many :disciplines, :through => :grid_disciplines
+  has_many :grid_disciplines, dependent: :destroy
+  # has_many :disciplines, :through => :grid_disciplines
 
   validates :year, :active, :course, presence:true
 
-  accepts_nested_attributes_for :grid_disciplines, allow_destroy: true
+  accepts_nested_attributes_for :grid_disciplines, :reject_if => :all_blank, :allow_destroy => true
 
   def decorate
     @decorate ||= GridDecorator.new self
