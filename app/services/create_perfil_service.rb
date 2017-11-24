3b19c84adce6b281f0b6cb398b31a06800b69fdb
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2017-11-24 19:04:48 -0200

    roles

--- app/services/create_perfil_service.rb
+++ app/services/create_perfil_service.rb
@@ -11,5 +11,18 @@ class CreatePerfilService
     end
 
     puts "Lista de perfis atualizada!"
+
+    models ||= []
+    ActiveRecord::Base.connection.tables.each do |v|
+      models << v.singularize.camelize unless ['ArInternalMetadatum', 'SchemaMigration'].include?(v.singularize.camelize)
+    end
+    models.sort
+
+    models.each do |m|
+      Role.find_or_create_by!( name: "Gerenciar #{m}", resource_type: m.to_s, resource_id: 'manage' ) unless m.nil?
+    end
+
+
+
   end
 end
