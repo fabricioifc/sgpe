4d64a244d9517420871edb3a5262768a6a71e5dd
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2017-11-28 16:02:49 -0200

    commit

--- app/services/create_users_service.rb
+++ app/services/create_users_service.rb
@@ -4,12 +4,12 @@ class CreateUsersService
 
     # Usuários NUPE, CGE, DDE com seus devidos perfis
     users = []
-    users << { name: 'Sandra de Fátima Lucietti', email: 'sandra.lucietti@fraiburgo.ifc.edu.br', perfils: [Perfil.find_by(name: 'NUPE')] }
-    users << { name: 'Débora de Lima Velho Junges', email: 'debora.junges@ifc.edu.br', perfils: [Perfil.find_by(name: 'NUPE')] }
-    users << { name: 'Pedro dos Santos Faccin', email: 'pedro.faccin@ifc.edu.br', perfils: [Perfil.find_by(name: 'NUPE')] }
-    users << { name: 'Coordenação Geral de Ensino', email: 'cge@fraiburgo.ifc.edu.br', perfils: [Perfil.find_by(name: 'DDE'), Perfil.find_by(name: 'CGE')] }
-    users << { name: 'Tiago Gonçalves', email: 'tiago.goncalves@ifc.edu.br', admin:true, teacher:true, perfils: [Perfil.find_by(name: 'DDE')] }
-    users << User.new(name: 'Paulo Roberto Ribeiro Nunes', email: 'paulo.nunes@fraiburgo.ifc.edu.br', perfils: [Perfil.find_by(name: 'NUPE')])
+    users << User.new( name: 'Sandra de Fátima Lucietti', email: 'sandra.lucietti@fraiburgo.ifc.edu.br', perfils: [Perfil.find_by(name: 'NUPE')] )
+    users << User.new( name: 'Débora de Lima Velho Junges', email: 'debora.junges@ifc.edu.br', perfils: [Perfil.find_by(name: 'NUPE')] )
+    users << User.new( name: 'Pedro dos Santos Faccin', email: 'pedro.faccin@ifc.edu.br', perfils: [Perfil.find_by(name: 'NUPE')] )
+    users << User.new( name: 'Coordenação Geral de Ensino', email: 'cge@fraiburgo.ifc.edu.br', perfils: [Perfil.find_by(name: 'DDE'), Perfil.find_by(name: 'CGE')] )
+    users << User.new( name: 'Tiago Gonçalves', email: 'tiago.goncalves@ifc.edu.br', admin:true, teacher:true, perfils: [Perfil.find_by(name: 'DDE')] )
+    users << User.new( name: 'Paulo Roberto Ribeiro Nunes', email: 'paulo.nunes@fraiburgo.ifc.edu.br', perfils: [Perfil.find_by(name: 'NUPE')])
 
     users.each do |u, v|
       u.username               = u.email.split('@')[0]
