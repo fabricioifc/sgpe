06289bc8d91e059025027931f221b4f0ff5ecff3
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2018-03-09 15:21:15 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+# user = CreateAdminService.new.call
+# puts 'CREATED ADMIN USER: ' << user.email
 
 # Criar os modalidades, formatos e cursos
-CreateCourseService.new.call
+# CreateCourseService.new.call
 # Criar disciplinas
-CreateDisciplineService.new.call
+# CreateDisciplineService.new.call
 # Criar perfis
-CreatePerfilService.new.call
+# CreatePerfilService.new.call
 # Criar usuários padrão
-CreateUsersService.new.call
+# CreateUsersService.new.call
