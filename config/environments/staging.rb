c02d7132494e83e66c4ff4ff89ae98b0c1442334
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2018-04-03 20:05:48 -0300

    staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,5 +4,6 @@ require File.expand_path('../production.rb', __FILE__)
 Rails.application.configure do
   # Here override any defaults
   config.serve_static_files = true
-  config.active_job.queue_adapter = :inline
+  config.active_job.queue_adapter = :async
+  config.action_mailer.delivery_method = :letter_opener
 end
