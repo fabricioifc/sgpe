f5985d89e8f558b6ea33a30fd53bebfd0ab06135
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2018-04-05 12:28:53 -0300

    ajuste visitantes tela

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -11,5 +11,11 @@ class VisitorsController < ApplicationController
     #   @cursos = Course.where(id: curso_ids)
     # end
 
+    @class_panel1 = 'col-md-12'
+    @class_panel2 = ''
+
+    @class_panel1 = 'col-md-8' if (is_professor?) || (can? :aprovar_reprovar, Plan)
+    @class_panel2 = 'col-md-4' if (is_professor?) || (can? :aprovar_reprovar, Plan)
+
   end
 end
