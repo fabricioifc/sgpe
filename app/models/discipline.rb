4f93eceeef04677461d36eedf524a5cd9e17d245
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2018-04-05 11:23:38 -0300

    disciplinas

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -4,7 +4,7 @@ class Discipline < ApplicationRecord
   # has_many :grids#, :through => :grid_disciplines
 
   validates :title, :sigla, :user, presence:true
-  validates :title, :sigla, uniqueness: {scope: [:title, :sigla]}
+  validates :title, uniqueness:true
 
   def decorate
     @decorate ||= DisciplineDecorator.new self
