1e570abc697f824cdfcc5b7672564e11fee7e744
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2021-05-28 00:37:25 -0300

    comentada linha com f.hidden_field :avatar

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,26 +1,30 @@
-class InvitationsController < Devise::InvitationsController
+# class InvitationsController < Devise::InvitationsController
 
-  before_action :update_sanitized_params
+#   before_action :update_sanitized_params
 
-  # PUT /resource/invitation
-  def update
-    respond_to do |format|
-      format.js do
-        invitation_token = Devise.token_generator.digest(resource_class, :invitation_token, update_resource_params[:invitation_token])
-        self.resource = resource_class.where(invitation_token: invitation_token).first
-        resource.skip_password = true
-        resource.update_attributes update_resource_params.except(:invitation_token)
-      end
-      format.html do
-        super
-      end
-    end
-  end
+#   # PUT /resource/invitation
+#   def update
+    
+#     binding.pry
+    
+#     respond_to do |format|
+#       format.js do
+#         invitation_token = Devise.token_generator.digest(resource_class, :invitation_token, update_resource_params[:invitation_token])
+#         self.resource = resource_class.where(invitation_token: invitation_token).first
+#         resource.skip_password = true
+#         resource.update_attributes update_resource_params.except(:invitation_token)
+#       end
+#       format.html do
+#         super
+#       end
+#     end
+#   end
 
 
-  protected
+#   protected
 
-  def update_sanitized_params
-    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:username, :email, :name, :password, :password_confirmation, :invitation_token, :avatar, :perfils, :perfil_ids => []])
-  end
-end
+#   def update_sanitized_params
+#     binding.pry
+#     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:username, :email, :name, :password, :password_confirmation, :invitation_token, :avatar, :perfils, :perfil_ids => []])
+#   end
+# end
